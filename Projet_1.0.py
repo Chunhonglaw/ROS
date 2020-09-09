8d0e3b7f795f2783e3e65666fa13b86a1e138b53
Chun Hong Law <law.chunhong@irsst.qc.ca>
2020-09-09 10:05:13 -0400

    Add Projet_1.0.py file

---
+++ Projet_1.0.py
@@ -0,0 +1,10 @@
+
+class Dog:                          # Création d'une classe Dog
+    def __init__(self, name):       
+        self.name = name
+
+    def get_name (self):            # méthode get_name
+        return self.name
+    
+Fido = Dog ('Fido')                 # Créer l'objet Fido                 
+print (Fido.get_name())     
